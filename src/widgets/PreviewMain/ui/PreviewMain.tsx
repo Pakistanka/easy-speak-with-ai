721a7f8fc5743babcd0fddfc858dfa52a3983bcd
Mariya <impa1002@gmail.com>
2025-11-13 00:04:08 +0300

    добавлен перевод главной

--- src/widgets/PreviewMain/ui/PreviewMain.tsx
+++ src/widgets/PreviewMain/ui/PreviewMain.tsx
@@ -1,15 +1,24 @@
 import { Button } from '@/shared';
 import styles from './PrevieMain.module.css';
+import { useTranslations } from 'next-intl';
+
 
 export const PreviewMain=()=>{
+    const t = useTranslations('homePage');
 
     return <div>
         <section className="home-1 py-15 text-center">
             <div className="container max-w-[1304px] mx-auto px-[19px] xl:px-0">
                 <div className="lg:w-[1080] m-auto">
-                    <h1 className="text-4xl font-bold lg:text-6xl leading-[150%] pb-6  " >Learn languages easily with <span className="text-(--color-span-e)">E</span>asy<span className="text-(--color-span-s)">S</span>peak - start your journey today</h1>
-                    <p className='leading-[150%] pb-10 text-[21px]'>Our language learning application offers an anonymous and secure environment for teachers and students. With crypto payments, you can enjoy the benefits of a decentralized currency without the need for traditional banking information.</p>
-                    <Button variant="primary" size="figma_md">Get started</Button>
+                    <h1 className="text-4xl font-bold lg:text-6xl leading-[150%] pb-6">
+                    {t('titleSection1_part1')}{' '}
+                    <span className="text-(--color-span-e)">{t('titleSection1_letterE')}</span>
+                    {t('titleSection1_middle')}
+                    <span className="text-(--color-span-s)">{t('titleSection1_letterS')}</span>
+                    {t('titleSection1_part2')}
+                    </h1>
+                    <p className='leading-[150%] pb-10 text-[21px]'>{t('descriptionSection1')}</p>
+                    <Button variant="primary" size="figma_md">{t('buttongetStarted')}</Button>
                 </div>
             </div>
         </section>
@@ -21,23 +30,23 @@ export const PreviewMain=()=>{
                             src="/images/img-pay.webp" 
                             alt="Описание" 
                         />
-                        <p className="text-[21px] xl:text-[27px] font-bold pb-4 wrap-break-word">Pay with cryptocurrency</p>
-                        <p className='text-[16px] xl:text-[20px] wrap-break-word'>Effortless transactions: embrace the future of payment</p>
+                        <p className="text-[21px] xl:text-[27px] font-bold pb-4 wrap-break-word">{t('blockPayTitle')}</p>
+                        <p className='text-[16px] xl:text-[20px] wrap-break-word'>{t('blockPayDescription')}</p>
                     </div>
                     <div className="flex flex-col w-full max-w-[352px] xl:max-w-none h-[374px] xl:h-[440px] bg-(--block-learn-background) rounded-[40px] px-8 py-5 text-white">
                         <img className='pb-8'
                             src="/images/img-learn.webp" 
                             alt="Описание" 
                         />
-                        <p className="text-[21px] xl:text-[27px] font-bold pb-4 wrap-break-word">Learn languages anonymously</p>
-                        <p className='text-[16px] xl:text-[20px] wrap-break-word'>Experience the benefits of anonymous language learning.</p>
+                        <p className="text-[21px] xl:text-[27px] font-bold pb-4 wrap-break-word">{t('blockLearnTitle')}</p>
+                        <p className='text-[16px] xl:text-[20px] wrap-break-word'>{t('blockLearnDescription')}</p>
                     </div>
                     <div className="grid grid-cols-1 w-full max-w-[352px] xl:max-w-none h-auto min-h-[402px] xl:h-[440px] bg-[url('/images/img-wrapper.webp')] bg-cover bg-center p-4 md:p-6 text-white rounded-[40px]">
                         <div className="row-1">
-                            <p className="text-[21px] xl:text-[27px] font-bold wrap-break-word">Discover the power of our app</p>
+                            <p className="text-[21px] xl:text-[27px] font-bold wrap-break-word">{t('blockDiscoverTitle')}</p>
                         </div>
                         <div className="row-2">
-                            <p className='pb-4 lg:pb-8 text-[16px] xl:text-[20px] wrap-break-word'>Learn at your own pace with our easy-to-use interface.</p>
+                            <p className='pb-4 lg:pb-8 text-[16px] xl:text-[20px] wrap-break-word'>{t('blockDiscoverDescription')}</p>
                         </div>
                         <div className="row-3 grid grid-cols-1 lg:grid-cols-[1fr_auto] gap-4 lg:gap-0">
                             <div className="col-1 flex justify-center lg:justify-start">
@@ -47,7 +56,7 @@ export const PreviewMain=()=>{
                                 />
                             </div>
                             <div className="col-2 flex items-end justify-center lg:justify-start lg:pl-7 xl:pl-10 pb-2.5 text-center ">
-                                <p className="w-full max-w-[110px] sm:max-w-[110px] lg:max-w-[210px] text-[16px] lg:text-[16px] xl:text-[21px] font-bold">Begin your language journey today!</p>
+                                <p className="w-full max-w-[110px] sm:max-w-[110px] lg:max-w-[210px] text-[16px] lg:text-[16px] xl:text-[21px] font-bold">{t('blockDiscoverBegin')}</p>
                             </div>
                         </div>
                     </div>
@@ -57,11 +66,11 @@ export const PreviewMain=()=>{
         <section className="home-3 pb-30">
             <div className="container max-w-[1304px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 px-[19px] lx:px-0">
                 <div>
-                    <h2 className="text-[27px] leading-[150%]  lg:text-5xl font-bold pb-6">Discover our innovative Language Learning App</h2>
-                    <p className='sm:text-[16px] lg:text-[21px] '>Our application offers a range of benefits for both teachers and students. Teachers can easily create and manage their language courses, while students can enjoy a seamless learning experience with speaking lessons and personalized feedback.</p>
+                    <h2 className="text-[27px] leading-[150%]  lg:text-5xl font-bold pb-6">{t('titleSection3')}</h2>
+                    <p className='sm:text-[16px] lg:text-[21px] '>{t('descriptionSection3')}</p>
                 </div>
                 <div>
-                    <img src="/images/phone-girl.webp" alt="Описание" />
+                    <img src="/images/phone-girl.webp" alt="icon" />
                 </div>
                 
             </div>
@@ -69,23 +78,23 @@ export const PreviewMain=()=>{
         <section className="home-4 pb-30 text-center">
             <div className="container max-w-[1304px] mx-auto px-[19px] xl:px-0">
                 <div className="text-[27px] md:text-[30px] lg:text-[47px] font-bold pb-10 md:pb-14">
-                    <p>Get started in 3 simple steps</p>
+                    <p>{t('titleSection4')}</p>
                 </div>
                 <div className="div grid grid-cols-1 gap-[40] sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3">
                     <div className='div'>
                         <div className={`${styles['wrapp-stepNumber']} ml-[150]  relative flex justify-center items-center w-[41px] md:w-[54px] h-[41px] md:h-[54px] text-(--color-number-stepNumber) font-normal text-[27px] md:text-[36px] rounded-full border-[1]border-(--home-4-stepNumber-color) bg-(--home-4-stepNumber-color) ${styles.stepNumber}`}>1</div>
-                        <h3 className="text-[21px] md:text-[27px] font-bold pt-6 pb-4 md:pb-6">Join our language learning community today!</h3>
-                        <p className='text-[16px] md:text-[21px]'>Signing up is quick and easy. Begin your journey by creating a profile. The app helps students explore lessons and  teachers showcase their expertise.</p>
+                        <h3 className="text-[21px] md:text-[27px] font-bold pt-6 pb-4 md:pb-6">{t('blockStep1Title')}</h3>
+                        <p className='text-[16px] md:text-[21px]'>{t('blockStep1Description')}</p>
                     </div>
                     <div className="div">
                         <div className={`${styles['wrapp-stepNumber']} ml-[150] lg:m-auto mb-6 relative flex justify-center items-center w-[41px] md:w-[54px] h-[41px] md:h-[54px] text-(--color-number-stepNumber) font-normal text-[27px] md:text-[36px] rounded-full border-[1]border-(--home-4-stepNumber-color) bg-(--home-4-stepNumber-color) ${styles.stepNumber}`}>2</div>
-                        <h3 className="text-[21px] md:text-[27px] font-bold pt-6 pb-4 md:pb-6">Explore lessons and connect with the right match</h3>
-                        <p className='text-[16px] md:text-[21px]'>Discover a diverse range of lessons. For students, find the perfect teacher, and for teachers, showcase your lessons to connect with eager learners.</p>
+                        <h3 className="text-[21px] md:text-[27px] font-bold pt-6 pb-4 md:pb-6">{t('blockStep2Title')}</h3>
+                        <p className='text-[16px] md:text-[21px]'>{t('blockStep2Description')}</p>
                     </div>
                     <div className="div">
                         <div className={`${styles['wrapp-stepNumber']} ml-[150] mb-6 relative flex justify-center items-center w-[41px] md:w-[54px] h-[41px] md:h-[54px] text-(--color-number-stepNumber) font-normal text-[27px] md:text-[36px] rounded-full border-[1] border-(--home-4-stepNumber-color) bg-(--home-4-stepNumber-color) ${styles.stepNumber} ${styles.stepNumberThird}`}>3</div>
-                        <h3 className="text-[21px] md:text-[27px] font-bold pt-6 pb-4 md:pb-6">Start your journey to learn a new language</h3>
-                        <p className='text-[16px] md:text-[21px]'>Finalize your connection by booking lessons (for students) or getting booked (for teachers). Pay with crypto and start the learning journey!</p>
+                        <h3 className="text-[21px] md:text-[27px] font-bold pt-6 pb-4 md:pb-6">{t('blockStep3Title')}</h3>
+                        <p className='text-[16px] md:text-[21px]'>{t('blockStep3Description')}</p>
                     </div>
                 </div>
             </div>
